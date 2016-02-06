64b2c0523e547026840eed8ac10b295471f70038
Christopher Tang <ctang18@gmail.com>
2016-02-06 06:48:37 -0500

    Reorganize tests

--- test/middleware/api/test-mw-get-content.js
+++ test/middleware/api/test-mw-get-content.js
@@ -2,13 +2,13 @@ var chai = require('chai');
 var should = chai.should();
 var mongoose = require('mongoose');
 
-var User = require('../../server/models/user');
-var Content = require('../../server/models/content');
+var User = require('../../../server/models/user');
+var Content = require('../../../server/models/content');
 
 var passport = require('passport');
 var jwt = require('jsonwebtoken');
 
-var loginMW = require('../../server/routes/login');
+var loginMW = require('../../../server/routes/login');
 
 describe('Get Content Middleware', function() {
   it('should 200 and return all contents given authorized user');
--- test/middleware/test-mw-login.js
+++ test/middleware/test-mw-login.js
@@ -1,16 +1,32 @@
 var chai = require('chai');
 var should = chai.should();
 var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 var User = require('../../server/models/user');
 var Content = require('../../server/models/content');
-
-var passport = require('passport');
-var jwt = require('jsonwebtoken');
-
 var loginMW = require('../../server/routes/login');
 
+passport.use(new LocalStrategy(User.authenticate()));
+
 describe('Login Middleware', function() {
-  it('should 200 on successful login authorization');
-  it('should 401 on wrong username/password');
+  it('should 200 on successful login authorization', function(done){
+    done();
+  });
+  it('should 401 on wrong username/password combination', function(done){
+    var req = {
+      body: {
+        username: 'username@test.com',
+        password: 'password'
+      }
+    };
+    var res = new Response();
+    loginMW(req, res, function(err){
+      done();
+    })
+  });
+  it('should 401 on non-existant user');
 });
